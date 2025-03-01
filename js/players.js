2256248ad25213dd44e72fed43313de6bc8a9299
ImChill1n <69574237+ImChill1n@users.noreply.github.com>
2025-03-01 18:44:46 +0100

    Update players.js

--- js/players.js
+++ js/players.js
@@ -117,7 +117,7 @@ document.getElementById("search-button").addEventListener("click", async () => {
         if (user_info.ok) {
             resultMessage.textContent = "Success";
             resultMessage.style.color = "green";
-            ign.innerHTML = `<b>IGN:</b> ${us_info.nick}`;
+            ign.innerHTML = `<b>IGN:</b> ${us_info.Name}`;
             country.innerHTML = `<b>Country:</b> ${us_info.CountryCode || "Unknown"}`;
             avatar.src = `https://vzge.me/full/${name}`;
 
