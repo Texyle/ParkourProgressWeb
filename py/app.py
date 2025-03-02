9f192c14e183369d7d47a7881e1272085dae0a12
Texyle <texyle@mail.ru>
2025-03-02 16:11:27 +0300

    fix connection

--- py/app.py
+++ py/app.py
@@ -1,5 +1,5 @@
 import os
-import mysql.connector
+from mysql.connector import Error
 from flask import Flask, request, jsonify, send_from_directory
 import pycountry
 from datetime import datetime
@@ -13,120 +13,137 @@ IMAGES_DIR = os.path.join(PROJECT_DIR, "images")
 app = Flask(__name__, static_url_path="", static_folder=PROJECT_DIR)
 
 def check_player_exists(name):
-    cursor = connection.get_cursor()
-    
-    query = "SELECT 1 FROM Player WHERE Name = %s LIMIT 1"
-    cursor.execute(query, (name,))
-    player = cursor.fetchone()
-    
-    cursor.close()
-    
-    return player
+    try:
+        cursor = connection.get_cursor()
+        
+        query = "SELECT 1 FROM Player WHERE Name = %s LIMIT 1"
+        cursor.execute(query, (name,))
+        player = cursor.fetchone()
+        
+        cursor.close()
+        
+        return player
+    except Error:
+        app.logger.error(e.msg)
 
 def load_player_info(name):
-    cursor = connection.get_cursor(dictionary=True)
+    try:
+        cursor = connection.get_cursor(dictionary=True)
 
-    query = """
-    SELECT Player.DiscordID, Player.CountryCode, Player.Name AS Nick
-    FROM Player 
-    WHERE Player.Name = %s
-    """
+        query = """
+        SELECT Player.DiscordID, Player.CountryCode, Player.Name AS Nick
+        FROM Player 
+        WHERE Player.Name = %s
+        """
 
-    cursor.execute(query, (name,))
-    info = cursor.fetchone()
-    cursor.close()
+        cursor.execute(query, (name,))
+        info = cursor.fetchone()
+        cursor.close()
 
-    if info is None:
-        return None
-    
-    id = info.get("CountryCode")
-    if id == "idk":
-        info['CountryCode'] == "Unknown"
-    cc = pycountry.countries.get(alpha_2=id)
-    info['CountryCode'] = cc.name 
+        if info is None:
+            return None
+        
+        id = info.get("CountryCode")
+        if id == "idk":
+            info['CountryCode'] == "Unknown"
+        cc = pycountry.countries.get(alpha_2=id)
+        info['CountryCode'] = cc.name 
 
-    return info
+        return info
+    except Error as e:
+        app.logger.log(e.msg)
 
 def get_maps_in_prog(name):
-    cursor = connection.get_cursor(dictionary=True)
+    try:
+        cursor = connection.get_cursor(dictionary=True)
 
-    query = """
-    SELECT Map.Name AS MapName, Section.Name AS SectionName
-    FROM SectionPlayer
-    JOIN Player
-    ON Player.ID = SectionPlayer.PlayerID
-    JOIN Section
-    ON Section.ID = SectionPlayer.SectionID
-    JOIN Map
-    ON Map.ID = Section.MapID
-    WHERE Player.Name = %s
-    """
+        query = """
+        SELECT Map.Name AS MapName, Section.Name AS SectionName
+        FROM SectionPlayer
+        JOIN Player
+        ON Player.ID = SectionPlayer.PlayerID
+        JOIN Section
+        ON Section.ID = SectionPlayer.SectionID
+        JOIN Map
+        ON Map.ID = Section.MapID
+        WHERE Player.Name = %s
+        """
 
-    cursor.execute(query, (name,))
-    prog = cursor.fetchall()
-    cursor.close()
+        cursor.execute(query, (name,))
+        prog = cursor.fetchall()
+        cursor.close()
 
-    if prog is None:
-        return None
-    
-    return prog
+        if prog is None:
+            return None
+        
+        return prog
+    except Error as e:
+        app.logger.log(e.msg)
     
 def get_victor_maps(*args):
-    cursor = connection.get_cursor(dictionary=True)
-    name = args[0]
-    gamemode = args[1]
+    try:
+        cursor = connection.get_cursor(dictionary=True)
+        name = args[0]
+        gamemode = args[1]
 
-    query = """
-SELECT Map.Name AS MapName, Player.Name AS PlayerName, Victor.Date AS Date, Victor.Fails AS Fails, Map.FailsMessage AS FailsMessage
-FROM Victor
-JOIN Map
-ON Map.ID = Victor.MapID
-JOIN Player
-ON Player.ID = Victor.PlayerID
-JOIN Gamemode
-ON Gamemode.ID = Map.GamemodeID
-WHERE Player.Name = %s
-AND Gamemode.Name = %s
-ORDER BY Date ASC
-"""
+        query = """
+        SELECT Map.Name AS MapName, Player.Name AS PlayerName, Victor.Date AS Date, Victor.Fails AS Fails, Map.FailsMessage AS FailsMessage
+        FROM Victor
+        JOIN Map
+        ON Map.ID = Victor.MapID
+        JOIN Player
+        ON Player.ID = Victor.PlayerID
+        JOIN Gamemode
+        ON Gamemode.ID = Map.GamemodeID
+        WHERE Player.Name = %s
+        AND Gamemode.Name = %s
+        ORDER BY Date ASC
+        """
 
-    cursor.execute(query, (name, gamemode))
-    victor = cursor.fetchall()
-    cursor.close()
+        cursor.execute(query, (name, gamemode))
+        victor = cursor.fetchall()
+        cursor.close()
 
-    if victor is None:
-        return None
+        if victor is None:
+            return None
+        
+        for entry in victor:
+            date_obj = entry['Date']
+            formatted_date = date_obj.strftime('%b %d, %Y')  
+            entry['Date'] = formatted_date  
+        
+        return victor
     
-    for entry in victor:
-        date_obj = entry['Date']
-        formatted_date = date_obj.strftime('%b %d, %Y')  
-        entry['Date'] = formatted_date  
-    
-    return victor
-
+    except Error as e:
+        app.logger.log(e.msg)
+        
 def load_latest_victors():
-    cursor = connection.get_cursor()
-    
-    query = """
-    SELECT Player.Name AS PlayerName, Map.Name AS MapName, Victor.Date AS Date FROM Victor
-    JOIN Player
-    ON Player.ID = Victor.PlayerID
-    JOIN Map
-    ON Map.ID = Victor.MapID
-    ORDER BY Date DESC
-    LIMIT 5
-    """
-    cursor.execute(query)
-    victors = cursor.fetchmany(5)
-    
-    cursor.close()
+    try:
+        cursor = connection.get_cursor()
+        
+        query = """
+        SELECT Player.Name AS PlayerName, Map.Name AS MapName, Victor.Date AS Date FROM Victor
+        JOIN Player
+        ON Player.ID = Victor.PlayerID
+        JOIN Map
+        ON Map.ID = Victor.MapID
+        ORDER BY Date DESC
+        LIMIT 5
+        """
+        cursor.execute(query)
+        victors = cursor.fetchmany(5)
+        
+        cursor.close()
 
-    victors_list = [
-        {"player_name": row[0], "map_name": row[1], "date": row[2].isoformat()} 
-        for row in victors
-    ]
+        victors_list = [
+            {"player_name": row[0], "map_name": row[1], "date": row[2].isoformat()} 
+            for row in victors
+        ]
 
-    return victors_list
+        return victors_list
+    
+    except Error as e:
+        app.logger.log(e.msg)
 
 @app.route("/check_player", methods=["POST"])
 def check_player():
