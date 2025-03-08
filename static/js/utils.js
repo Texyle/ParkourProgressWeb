c5db0684de3d68bbfd81e88a193e2322cd171fd5
ImChill1n <rektdrifter2@gmail.com>
2025-03-08 17:51:20 +0100

    fix jinja

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,30 +1,45 @@
 async function loadMapImage(mapName) {
-  const baseFileName = mapName.replace(/[\W\s]/g, '').toLowerCase();
-  const pngFileName = `${baseFileName}.png`;
-  const jpgFileName = `${baseFileName}.jpg`;
+    const baseFileName = mapName.replace(/[\W\s]/g, '').toLowerCase();
+    const pngFileName = `${baseFileName}.png`;
+    const jpgFileName = `${baseFileName}.jpg`;
 
-  return new Promise((resolve, reject) => {
-      const img = new Image();
+    return new Promise((resolve, reject) => {
+        const img = new Image();
 
-      img.onload = function() {
-          resolve(`/static/images/maps/${pngFileName}`);
-      };
+        img.onload = function() {
+            resolve(`/static/images/maps/${pngFileName}`);
+        };
 
-      img.onerror = function() {
-          const jpgImg = new Image();
-          jpgImg.onload = function() {
-              resolve(`/static/images/maps/${jpgFileName}`);
-          };
-          jpgImg.onerror = function() {
-              reject(new Error('Neither PNG nor JPG file exists'));
-          };
-          jpgImg.src = `/static/images/maps/${jpgFileName}`;
-      };
+        img.onerror = function() {
+            const jpgImg = new Image();
+            jpgImg.onload = function() {
+                resolve(`/static/images/maps/${jpgFileName}`);
+            };
+            jpgImg.onerror = function() {
+                reject(new Error(`Image not found: ${mapName}`));
+            };
+            jpgImg.src = `/static/images/maps/${jpgFileName}`;
+        };
 
-      img.src = `/static/images/maps/${pngFileName}`;
-  });
+        img.src = `/static/images/maps/${pngFileName}`;
+    });
+}
+
+async function loadAllMapImages() {
+    const images = document.querySelectorAll('.map-image');
+
+    for (const img of images) {
+        const mapName = img.dataset.mapName;
+        try {
+            const imageUrl = await loadMapImage(mapName);
+            img.src = imageUrl;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 
 window.Utils = {
-  loadMapImage
+    loadMapImage,
+    loadAllMapImages
 };
