7746671c49c5debebacdf8d0f3830396f2b0adf1
ImChill1n <rektdrifter2@gmail.com>
2025-03-21 14:25:03 +0100

    again

--- static/js/pagetitle.js
+++ static/js/pagetitle.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     let baseTitle = document.title;
-    let parts = baseTitle.split(" - "); 
-    let mainPart = parts[0]; 
-    let suffix = " - " + parts.slice(1).join(" - "); 
-    let speed = 200;  
+    let speed = 200;
     let index = 0;
 
+    let extendedTitle = " - " + baseTitle + " - ";
+
     function scrollTitle() {
-        document.title = mainPart.substring(index) + " " + mainPart.substring(0, index) + suffix;
-        index = (index + 1) % mainPart.length;
+        document.title = extendedTitle.substring(index) + extendedTitle.substring(0, index);
+        index = (index + 1) % extendedTitle.length;
         setTimeout(scrollTitle, speed);
     }
 
