ce816e9d125f0be15be18f930ae1d2728f0fc5b5
ImChill1n <rektdrifter2@gmail.com>
2025-03-21 21:31:24 +0100

    add background changer

---
+++ static/js/bgchanger.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function changeBackground() {
+        fetch("/random-image")
+            .then(response => response.json())
+            .then(data => {
+                if (data.image_url) {
+                    document.body.style.backgroundImage = `url('${data.image_url}')`;
+                }
+            })
+            .catch(error => console.error("Error fetching image:", error));
+    }
+
+    setTimeout(() => {
+        changeBackground();
+        setInterval(changeBackground, 20000);
+    }, 20000); 
+});
