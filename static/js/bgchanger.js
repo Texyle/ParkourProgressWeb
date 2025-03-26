5d1195844920a0576f11219b3de27eadc05ca143
Texyle <texyle@mail.ru>
2025-03-26 23:21:38 +0300

    bgchanger small fix

--- static/js/bgchanger.js
+++ static/js/bgchanger.js
@@ -1,25 +1,29 @@
+const interval = 20000;
+
 document.addEventListener("DOMContentLoaded", function () {
     function changeBackground() {
         fetch("/random-image")
             .then(response => response.json())
             .then(data => {
-                // if (data.image_url) {
-                //     document.body.style.transition = "background-image 1.5s ease-in-out";
-                //     document.body.style.backgroundImage = `url('${data.image_url}')`;
-                // }
                 const img = new Image();
                 img.src = data.image_url;
 
                 img.onload = function () {
                     document.body.style.transition = "background-image 1.5s ease-in-out";
                     document.body.style.backgroundImage = `url('${data.image_url}')`;
+
+                    setTimeout(changeBackground, interval);
+                };
+
+                img.onerror = function () {
+                    setTimeout(changeBackground, interval);
                 };
             })
-            .catch(error => console.error("Error fetching image:", error));
+            .catch(error => {
+                console.error("Error fetching background image:", error);
+                setTimeout(changeBackground, interval);
+            });
     }
 
-    setTimeout(() => {
-        changeBackground();
-        setInterval(changeBackground, 20000);
-    }, 20000); 
+    setTimeout(changeBackground, interval);
 });
