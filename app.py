ffe112dae32bd1cde90fad263c2cad10a843a454
Texyle <texyle@mail.ru>
2025-05-14 21:57:39 +0300

    automatically create and use local .db file"

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from app import create_app
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    host = app.config.get('FLASK_RUN_HOST', '127.0.0.1')
-    port = app.config.get('FLASK_RUN_PORT', 20000)
-    debug = app.config.get('FLASK_DEBUG', True)
+if __name__ == "__main__":
+    host = app.config.get("FLASK_RUN_HOST", "127.0.0.1")
+    port = app.config.get("FLASK_RUN_PORT", 20000)
+    debug = app.config.get("FLASK_DEBUG", True)
+    
     app.run(host=host, port=port, debug=debug) 
